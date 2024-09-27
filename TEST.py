0e129afc317e2de2e9d87b00759fc06d785ca343
Sashanksasha <sashanik0623@gmail.com>
2024-09-26 23:55:13 -0500

    fortnite x1

--- TEST.py
+++ TEST.py
@@ -4,4 +4,10 @@ if hello != type(int):
     print("don't listen to me")
 else:
     print("1+1=4-2 is 2 quick math")
-print("Fortnite")
+
+x = int(input("Input a number for fortnite to print: "))
+y = 0
+
+while y < x:
+    y += 1
+    print("Fortnite")
